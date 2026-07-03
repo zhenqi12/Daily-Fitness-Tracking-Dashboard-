99c9f7aec809a031d6ad29704133bd28780c8745
zhenqi12 <25049650@myrp.edu.sg>
2026-07-03 09:18:07 +0000

    Remove all files except README.md

--- routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const dashboardController = require("../controllers/dashboardController");
-
-router.get("/", dashboardController.showDashboard);
-
-module.exports = router;
